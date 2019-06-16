0237c3e26bc757e77ed93a9faaecd99fdf393636
Matheus Marotzke <matheusmarotzke@Matheuss-MacBook-Pro.local>
2019-06-16 12:04:51 -0300

    MarketMaker

--- exampleMatheus.py
+++ exampleMatheus.py
@@ -1,4 +1,4 @@
-from backtesting import evaluateHist
+from backtesting import evaluateHist, evaluateIntr
 from strategy import Strategy
 from order import Order
 import numpy as np
@@ -108,5 +108,76 @@ class TheOneChoice(Strategy):
     self.prev = price
     return orders
 
+class MarketMaker(Strategy):
+  def __init__(self):
+    self.orders = []
+    self.two_orders = []
+    self.petr3 = None
+    self.usd_brl = None
+    self.spread = 50
+    self.order_ids = None
+    self.f = 2
+    self.ti = 1.01
+    self.tc = -0.035
+
+  def get_pbr(self, petr3, usd_brl):
+    #Ppbr = Ppetr3 * F(quantas pbr=petr3)/Pusr-brl * ti + tc
+    pbr = ((petr3 * self.f) / usd_brl) * self.ti + self.tc
+    return pbr
+
+  def push(self, event):
+    orders = []
+    #checa petr3
+    if event.instrument == "PETR3":
+        self.petr3 = event.price[3]
+
+    #checa usd_brl
+    if event.instrument == "USDBRL":
+        self.usd_brl = event.price[3]
+    
+    if self.petr3 and self.usd_brl:
+        #calcula pbr
+        pbr = self.get_pbr(self.petr3, self.usd_brl)
+
+        #calcula spread
+        buy_price = pbr - self.spread
+        sell_price = pbr + self.spread
+
+        #cancela atuais
+        if self.order_ids:
+            for order_id in self.order_ids:
+                self.cancel(self.id, self.order_id)
+
+        #manda duas novas ordens
+        self.order_ids = []
+        orders.append(Order("PBR", 1,buy_price))
+        orders.append(Order("PBR", -1,sell_price))
+        for order in orders:
+            self.order_ids.append(order.id)
+
+        return orders
+    return []
+
+    def fill(self, instrument, price, quantity, status):
+        super().fill(instrument, price, quantity, status)
+        orders = []
+
+        #só fill ids ordens de pbr
+        if instrument == "PBR":
+            #comprar pbr -> vende petr3, compra dolar (no preço que tiver)
+            if quantity == 1:
+                orders.append(Order("PETR3", -1, 0))
+                orders.append(Order("USDBRL", 1, 0))
+
+            #vender pbr -> compra petr3, vende dolar (no preço que tiver)
+            elif quantity == -1:
+                orders.append(Order("PETR3", 1, 0))
+                orders.append(Order("USDBRL", -1, 0))
+
+        #submit a mercado
+        return orders
+
+
 print(evaluateHist(EMA(), {'IBOV':'^BVSP.csv'}))
 print(evaluateHist(TheOneChoice(), {'IBOV':'^BVSP.csv'}))
+print(evaluateIntr(MarketMaker(), {'USDBRL':'USDBRL.csv', 'PETR3':'PETR3.csv'}))
